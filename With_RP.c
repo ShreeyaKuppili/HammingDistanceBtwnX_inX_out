24f04de24989c17bdc77c936c9e166cad757509a
ShreeyaKuppili <143098608+ShreeyaKuppili@users.noreply.github.com>
2023-10-31 21:52:08 +0530

    changed everything to float instead of **float

--- With_RP.c
+++ With_RP.c
@@ -11,16 +11,15 @@
 #define BITS_TO_COMPARE 25
 
 // Define the lengths for x_in and x_out
-
 #define X_IN_LENGTH 4000
 #define X_OUT_LENGTH 4000
 
-float *x_in[X_IN_LENGTH];
+float x_in[X_IN_LENGTH];
 float x_out[X_OUT_LENGTH];
 uint32_t buff_size = BUFFER_SIZE;
 
 // Function to calculate Hamming distance between two arrays
-int calculate_hamming_distance(float **x1, float **x2, int length) {
+int calculate_hamming_distance(const float *x1, const float *x2, int length) {
     int distance = 0;
 
     for (int i = 0; i < length; i++) {
@@ -32,18 +31,17 @@ int calculate_hamming_distance(float **x1, float **x2, int length) {
     return distance;
 }
 
-int find_min_hamming_position(float **x_in, float **x_out, int in_length, int out_length) {
+int find_min_hamming_position(const float *x_in, const float *x_out, int in_length, int out_length) {
     int min_distance = X_OUT_LENGTH + 1;
     int min_position = -1;
 
-    for(int i = 0; i < X_OUT_LENGTH - BITS_TO_COMPARE; i++)
-    {
+    for (int i = 0; i < X_OUT_LENGTH - BITS_TO_COMPARE; i++) {
         int distance = calculate_hamming_distance(x_in, &x_out[i], BITS_TO_COMPARE);
         if (distance < min_distance) {
             min_distance = distance;
             min_position = i;
         }
-        if(min_distance == 0) break;
+        if (min_distance == 0) break;
     }
     return min_position;
 }
@@ -52,7 +50,7 @@ int main(int argc, char **argv) {
     // Seed the random number generator
     srand(time(NULL));
 
-   // Initialize Red Pitaya resources
+    // Initialize Red Pitaya resources
     if (rp_Init() != RP_OK) {
         fprintf(stderr, "Red Pitaya initialization failed!\n");
         return 1;
@@ -61,27 +59,11 @@ int main(int argc, char **argv) {
     // Configure ADC
     rp_AcqReset();
     rp_AcqSetDecimation(1);
-    
-// Allocate memory for x_in and x_out
-    for (int i = 0; i < buff_size; i++) {
-    x_in[i] = (float *)malloc(sizeof(float));
-    x_out[i] = (float *)malloc(sizeof(float));
-}
-
-
-
-    if (x_in == NULL) {
-        fprintf(stderr, "Memory allocation for x_in failed!\n");
-        return 1;
-    }
 
     // Generate random data for DAC (x_out)
-    srand(time(NULL)); // Seed the random number generator
     for (int i = 0; i < buff_size; i++) {
-    x_out[i][0] = (float)rand() / RAND_MAX; // Generates random floating-point values between 0 and 1
-}
-
-
+        x_out[i] = (float)rand() / RAND_MAX; // Generates random floating-point values between 0 and 1
+    }
 
     // Perform data acquisition from Channel 2 (RP_CH_2) where the signal is routed externally
     rp_AcqStart();
@@ -91,15 +73,15 @@ int main(int argc, char **argv) {
         return 1;
     }
     rp_AcqStop();
+
     int min_position = find_min_hamming_position(x_in, x_out, BITS_TO_COMPARE * SAMPLES_PER_BIT, X_OUT_LENGTH);
     int min_distance = calculate_hamming_distance(x_in, &x_out[min_position], BITS_TO_COMPARE * SAMPLES_PER_BIT);
 
-
     printf("Minimum Hamming Distance: %d\n", min_distance);
     printf("Position with Minimum Distance: %d\n", min_position);
-// Release Red Pitaya resources and free memory
+
+    // Release Red Pitaya resources (no need to free x_in as it's not dynamically allocated)
     rp_Release();
-    free(x_in);
 
     return 0;
 }
